044e4490e4849100b57be9135508f73107755dae
brutalkingran <swkvideogames@gmail.com>
2025-12-01 22:36:16 -0300

    tamaño iconos solucionados (size removido) y solucion estilos borde

--- src/components/MainComponent.jsx
+++ src/components/MainComponent.jsx
@@ -7,7 +7,7 @@ import { FaInstagram } from "react-icons/fa";
 const MainComponent = () => {
   return (
     <main className="flex flex-col min-h-screen text-center justify-center">
-      <div className="m-1 border-40 flex flex-col items-center justify-center gap-6" id="borderimg3">
+      <div className="m-1 border-[40px] flex flex-col items-center justify-center gap-6" id="borderimg3">
         {/* LOGO GRANDE */}
         <div className="grid place-items-center">
           <img className="col-start-1 row-start-1" src={logo_text} alt="Texto de logo de Patrician Software" />
@@ -16,16 +16,16 @@ const MainComponent = () => {
 
         <div className="flex flex-col items-center justify-center">
           {/* TÍTULO */}
-          <h1 className="text-[2.7rem] text-center font-extralight font-serif">Patrician Software</h1>
+          <h1 className="text-[2.5rem] text-center font-extralight font-serif">Patrician Software</h1>
 
           {/* SUBTÍTULO */}
           <h2 className="text-[1.1rem] mx-6 text-center italic font-extralight">Expertos en Desarrollo de Software y Marketing Digital</h2>
         </div>
 
         <div className="flex flex-row justify-center items-center gap-5 mt-5">
-          <a href="https://wa.me/543834091942"><AiOutlineWhatsApp size={75} /></a>
-          <a href="instagram://user?username=patrician-software/"><FaInstagram size={75} /></a>
-          <a href="mailto:services@patrician-software.com/"><AiOutlineMail size={75} /></a>
+          <a href="https://wa.me/543834091942"><AiOutlineWhatsApp className="w-18 h-18" /></a>
+          <a href="instagram://user?username=patrician-software/"><FaInstagram className="w-18 h-18" /></a>
+          <a href="mailto:services@patrician-software.com/"><AiOutlineMail className="w-18 h-18" /></a>
         </div>
 
         <p className="text-center text-1xl italic">Contáctenos</p>
