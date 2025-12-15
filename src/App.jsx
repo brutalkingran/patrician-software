718657097af653865d0f8423208d90d727accedd
brutalkingran <swkvideogames@gmail.com>
2025-12-15 17:50:55 -0300

    actualizacion pixel

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 import MetaPixel from "./components/tracking/MetaPixel";
 import AppRouter from "./router/AppRouter";
+import { useEffect } from "react";
+import { initMetaPixel } from "./analytics/metaPixel";
 
 function App() {
+  useEffect(() => {
+    initMetaPixel();
+  }, []);
+
   return (
     <>
       <MetaPixel />
