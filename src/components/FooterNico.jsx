64dae069d0139767d4cfbffbe53d5b757f0b6392
brutalkingran <swkvideogames@gmail.com>
2026-09-07 15:01:38 -0300

    más colores y footer definicion mejorado

--- src/components/FooterNico.jsx
+++ src/components/FooterNico.jsx
@@ -16,16 +16,19 @@ const Footer = () => {
 
           {/* COLUMNA 1 */}
           <div>
-            <h2 className="text-2xl font-bold tracking-widest uppercase text-white">
-              PATRICIAN{" "}
-              <span className="text-[#38bdf8]">
-                SOFTWARE
-              </span>
+            <h2 className="pl-1 text-2xl font-bold tracking-widest bg-ps-white text-ps-blue">
+              <span className="text-ps-white [-webkit-text-stroke:1.5px_var(--color-ps-blue)]">P</span>atrician
+              <span className="text-ps-white [-webkit-text-stroke:1.5px_var(--color-ps-blue)]"> S</span>oftware
             </h2>
 
-            <p className="mt-4 max-w-xs text-sm text-gray-300 leading-relaxed italic">
-              Figura suprema de la élite, forjada en la cima del orden social, cuya sola presencia encarna poder, prestigio y autoridad.
-            </p>
+            <dl className="md:max-w-full border-l-3 border-white-800 pl-2">
+              <dt className="text-1xl font-serif font-semibold tracking-wide">
+                Patrician <span className="italic text-sm">(sust.)</span>
+              </dt>
+              <dd className="mt-1 leading-relaxed font-light font-serif pl-3 text-sm">
+                Figura suprema de la élite, forjada en la cima del orden social, cuya sola presencia encarna poder, prestigio y autoridad.
+              </dd>
+            </dl>
           </div>
 
           {/* COLUMNA 2 */}
@@ -75,21 +78,21 @@ const Footer = () => {
 
             <div className="space-y-4 text-sm text-gray-300">
               <div className="flex items-start gap-3.5">
-                <div className="mt-1 text-[#38bdf8]">
+                <div className="mt-1 text-ps-lblue">
                   <FaMapMarkerAlt size={15} />
                 </div>
                 <span>Catamarca, Argentina</span>
               </div>
 
               <div className="flex items-start gap-3.5">
-                <div className="mt-1 text-[#38bdf8]">
+                <div className="mt-1 text-ps-lblue">
                   <FaEnvelope size={15} />
                 </div>
                 <span className="break-all">contacto@patrician.com</span>
               </div>
 
               <div className="flex items-start gap-3.5">
-                <div className="mt-1 text-[#38bdf8]">
+                <div className="mt-1 text-ps-lblue">
                   <FaClock size={15} />
                 </div>
                 <span>Lun - Vie: 9:00 - 18:00</span>
@@ -108,14 +111,14 @@ const Footer = () => {
             © 2026 Patrician-software. Todos los derechos reservados.
           </p>
 
-          <div className="flex gap-6 text-xs text-gray-400">
+          {/* <div className="flex gap-6 text-xs text-gray-400">
             <a href="#" className="transition-colors hover:text-white">
               Política de Privacidad
             </a>
             <a href="#" className="transition-colors hover:text-white">
               Términos y Condiciones
             </a>
-          </div>
+          </div> */}
         </div>
 
       </div>
