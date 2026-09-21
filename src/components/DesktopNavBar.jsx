7ad79895d3b9bfef989c4d0759abbc0ceba6d139
brutalkingran <swkvideogames@gmail.com>
2026-09-21 19:48:38 -0300

    Z corregido navbar

--- src/components/DesktopNavBar.jsx
+++ src/components/DesktopNavBar.jsx
@@ -2,7 +2,7 @@ import { NAV_ITEMS } from "../data/navItems";
 
 const DesktopNavBar = () => {
   return (
-    <div className="hidden md:flex fixed top-0 inset-x-0 z-40 justify-center pointer-events-none">
+    <div className="hidden md:flex fixed top-0 inset-x-0 z-40 justify-center pointer-events-none z-999">
       <nav className="marco-imperial pointer-events-auto mt-4 flex items-center gap-5 lg:gap-7 px-6 py-2.5 bg-ps-blue/40 backdrop-blur-md border-10 shadow-lg">
         {NAV_ITEMS.map(({ href, label, highlight }) => (
           <a
